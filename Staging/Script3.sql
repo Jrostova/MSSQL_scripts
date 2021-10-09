a410e5c0a3ffe0e6d47bfd7760097ca2b8699f82
jrostova <judebendet@gmail.com>
2021-10-09 03:56:57 -0500

    Grafana

---
+++ Staging/Script3.sql
@@ -0,0 +1,16 @@
+﻿
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[GrafanaDemo](
+	[DataID] [int] IDENTITY(1,1) NOT NULL,
+	[Metric] [varchar](20) NULL,
+	[Value] [int] NULL,
+	[TimeStamp] [datetime] NULL
+) ON [PRIMARY]
+GO
+
+
